56860ca91237c97fb50e75fefb25c8db4d0c6a74
epicodus <student@epicodus.com>
2014-10-22 13:29:51 -0700

    Initial Commit

---
+++ FunFacts/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FunFacts
+//
+//  Created by epicodus on 10/22/14.
+//  Copyright (c) 2014 Ripelist. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
