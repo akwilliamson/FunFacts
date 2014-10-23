3f53aa8e1f7f00cd068554bfccc4ae447ad4577e
= <wikea1@gmail.com>
2014-10-22 17:50:53 -0700

    xcode project FunFacts

--- FunFacts/ViewController.swift
+++ FunFacts/ViewController.swift
@@ -9,10 +9,14 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet weak var funFactLabel: UILabel!
 
+    let factBook = FactBook()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        funFactLabel.text = factBook.randomFact()
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +24,9 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func showFunFact() {
+        funFactLabel.text = factBook.randomFact()
+    }
 
 }
 
---
+++ Scratchpad.playground/section-1.swift
@@ -0,0 +1,9 @@
+// Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+
+// Swift Recap
+
+var randomNumber = Int(arc4random_uniform(10))
